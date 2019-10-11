6b030f005346d3232cddc03a6449173cb271b8e8
dukezra1738 <54451452+dukezra1738@users.noreply.github.com>
2019-10-11 13:20:25 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+def sum(a,b):
+  add = a+b
+  return add 
+print(sum(2,3))
+
+
+def multi(a,b):
+  result = a*b
+  return result
+print(multi(2,3))
+
+
+
+def add():
+  x =int (input("enter 1st no: "))
+  y =int (input("enter 2nd no: "))
+  sum = x + y
+  return sum
+print(add())
